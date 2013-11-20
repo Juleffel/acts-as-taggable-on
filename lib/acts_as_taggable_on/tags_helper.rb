80d7c77bb376ea789f21fdbf028fac6439c52759
Juleffel <juleffel@hotmail.fr>
2013-11-20 20:53:50 +0100

    Update tags_helper.rb
    Add an int parameter number_max_of_tags to tag_cloud

--- lib/acts_as_taggable_on/tags_helper.rb
+++ lib/acts_as_taggable_on/tags_helper.rb
@@ -1,12 +1,16 @@
 module ActsAsTaggableOn
   module TagsHelper
     # See the README for an example using tag_cloud.
-    def tag_cloud(tags, classes)
+    def tag_cloud(tags, classes, number_max_of_tags)
       return [] if tags.empty?
 
-      max_count = tags.sort_by(&:count).last.count.to_f
+      sorted_tags = tags.sort_by(&:count).reverse
+      if (number_max_of_tags) {
+        sorted_tags = sorted_tags[0..(number_max_of_tags-1)]
+      }
+      max_count = sorted_tags.first.count.to_f
 
-      tags.each do |tag|
+      sorted_tags.shuffle.each do |tag|
         index = ((tag.count / max_count) * (classes.size - 1))
         yield tag, classes[index.nan? ? 0 : index.round]
       end
