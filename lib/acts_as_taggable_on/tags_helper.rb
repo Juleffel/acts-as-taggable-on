38202c9b1c9f4cd11c58e0d12ad8d2ac6a3812e8
Juleffel <juleffel@hotmail.fr>
2013-11-20 21:00:12 +0100

    Update tags_helper.rb
    Expanded values on tag_cloud.

--- lib/acts_as_taggable_on/tags_helper.rb
+++ lib/acts_as_taggable_on/tags_helper.rb
@@ -9,9 +9,13 @@ module ActsAsTaggableOn
         sorted_tags = sorted_tags[0..(number_max_of_tags-1)]
       end
       max_count = sorted_tags.first.count.to_f
+      min_count = sorted_tags.last.count.to_f
+      if max_count <= min_count
+        max_count = min_count + 1
+      end
 
       sorted_tags.shuffle.each do |tag|
-        index = ((tag.count / max_count) * (classes.size - 1))
+        index = (((tag.count - min_count) / (max_count - min_count)) * (classes.size - 1))
         yield tag, classes[index.nan? ? 0 : index.round]
       end
     end
